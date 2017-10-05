774e8838ca3abf69e07a193814e0744b106f6d92
Maxim <satanov201354@gmail.com>
2017-10-05 23:16:01 +0300

    Решил новые задачи в 3 уроке

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -60,7 +60,22 @@ fun digitCountInNumber(n: Int, m: Int): Int =
  * Найти количество цифр в заданном числе n.
  * Например, число 1 содержит 1 цифру, 456 -- 3 цифры, 65536 -- 5 цифр.
  */
-fun digitNumber(n: Int): Int = TODO()
+fun digitNumber(n: Int): Int
+{
+    if (n == 0)
+    {
+        return 1
+    }
+    var num = n
+    var a: Int = 0
+    var sum: Int = 0
+    while(num != 0)
+    {
+        sum += 1
+        num /= 10
+    }
+    return sum
+}
 
 /**
  * Простая
@@ -68,29 +83,88 @@ fun digitNumber(n: Int): Int = TODO()
  * Найти число Фибоначчи из ряда 1, 1, 2, 3, 5, 8, 13, 21, ... с номером n.
  * Ряд Фибоначчи определён следующим образом: fib(1) = 1, fib(2) = 1, fib(n+2) = fib(n) + fib(n+1)
  */
-fun fib(n: Int): Int = TODO()
-
+fun fib(n: Int): Int
+{
+    var i: Int = 2
+    var i1:Int = 1
+    var i2:Int = 1
+    var sum:Int = 0
+    if((n == 1) || (n == 2)) return 1
+    else
+    {
+        while(i < n)
+        {
+            sum = i1 + i2
+            i1 = i2
+            i2 = sum
+            i++
+        }
+    }
+    return sum
+}
 /**
  * Простая
  *
  * Для заданных чисел m и n найти наименьшее общее кратное, то есть,
  * минимальное число k, которое делится и на m и на n без остатка
  */
-fun lcm(m: Int, n: Int): Int = TODO()
+fun lcm(m: Int, n: Int): Int
+{
+    var n1 = n
+    var m1 = m
+    var dub = m1 * n1
+    while (m1 != n1)
+    {
+        if(m1 > n1)
+        {
+            m1 -= n1
+        }
+        else
+        {
+            n1 -= m1
+        }
+    }
+    return dub / m1
+}
 
 /**
  * Простая
  *
  * Для заданного числа n > 1 найти минимальный делитель, превышающий 1
  */
-fun minDivisor(n: Int): Int = TODO()
-
+fun minDivisor(n: Int):Int
+{
+    var i: Int = 2
+    var minden:Int = 1
+    var sqrtn:Int = Math.sqrt(n.toDouble()).toInt()
+    while (i <= sqrtn)
+    {
+        if(n % i == 0)
+        {
+            minden = i
+            break
+        }
+        i++
+    }
+    if(minden == 1) return n
+    else return minden
+}
 /**
  * Простая
  *
  * Для заданного числа n > 1 найти максимальный делитель, меньший n
  */
-fun maxDivisor(n: Int): Int = TODO()
+fun maxDivisor(n: Int): Int {
+    var maxden:Int = 1
+    for(i:Int in n-1 downTo 1)
+    {
+        if(n % i == 0) {
+            maxden = i
+            break
+        }
+    }
+    return maxden
+}
 
 /**
  * Простая
@@ -99,7 +173,24 @@ fun maxDivisor(n: Int): Int = TODO()
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean = TODO()
+fun isCoPrime(m: Int, n: Int): Boolean
+{
+    var m1:Int = m
+    var n1:Int = n
+    while(m1 !=0 && n1 !=0 )
+    {
+        if ( m1 >= n1)
+        {
+            m1 %= n1
+        }
+        else
+        {
+            n1 %= m1
+        }
+    }
+    m1 += n1
+    return m1 == 1
+}
 
 /**
  * Простая
@@ -134,7 +225,15 @@ fun cos(x: Double, eps: Double): Double = TODO()
  * Поменять порядок цифр заданного числа n на обратный: 13478 -> 87431.
  * Не использовать строки при решении задачи.
  */
-fun revert(n: Int): Int = TODO()
+fun revert(n: Int): Int {
+    var k: Int = 0
+    var n1 = n
+    while (n1 > 0){
+        k = k * 10 + (n1 % 10)
+        n1 /= 10
+    }
+    return k
+}
 
 /**
  * Средняя
@@ -143,7 +242,17 @@ fun revert(n: Int): Int = TODO()
  * первая цифра равна последней, вторая -- предпоследней и так далее.
  * 15751 -- палиндром, 3653 -- нет.
  */
-fun isPalindrome(n: Int): Boolean = TODO()
+fun isPalindrome(n: Int): Boolean
+{
+    var k: Int = 0
+    var n1 = n
+    while (n1 > 0){
+        k = k * 10 + (n1 % 10)
+        n1 /= 10
+    }
+    return k == n
+}
+
 
 /**
  * Средняя
@@ -151,7 +260,24 @@ fun isPalindrome(n: Int): Boolean = TODO()
  * Для заданного числа n определить, содержит ли оно различающиеся цифры.
  * Например, 54 и 323 состоят из разных цифр, а 111 и 0 из одинаковых.
  */
-fun hasDifferentDigits(n: Int): Boolean = TODO()
+fun hasDifferentDigits(n: Int): Boolean
+{
+    var main:Int = 0
+    var pred:Int = 0
+    var n1:Int = n
+    pred = n1 % 10
+    while(n1 != 0)
+    {
+        main = n1 % 10
+        if(main != pred)
+        {
+            return true
+        }
+        n1 /= 10
+        pred = main
+    }
+    return false
+}
 
 /**
  * Сложная
