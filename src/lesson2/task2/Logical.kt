e6701b55d68fbdbf8be930308a71452847bc869a
Maxim <satanov201354@gmail.com>
2017-10-05 23:15:03 +0300

    Решен 2 урок задание 2

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -2,6 +2,7 @@
 package lesson2.task2
 
 import lesson1.task1.sqr
+import sun.font.TrueTypeFont
 
 /**
  * Пример
@@ -17,7 +18,11 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean
+{
+    return number % 10 + number % 100 / 10 ==
+            number % 1000/100 + number / 1000
+}
 
 /**
  * Простая
@@ -26,7 +31,11 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean
+{
+    return (x1 + y1 == x2 + y2) || (x1 - y1 == x2 -y2)
+            || (x1 == x2) || (y1 == y2)
+}
 
 /**
  * Средняя
@@ -36,7 +45,8 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean = TODO()
+                 x2: Double, y2: Double, r2: Double): Boolean
+        = Math.sqrt(Math.pow((x2 - x1), 2.0) + Math.pow((y2 - y1), 2.0)) + r1 <= r2
 
 /**
  * Средняя
@@ -47,4 +57,7 @@ fun circleInside(x1: Double, y1: Double, r1: Double,
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean
+        =
+        ((a <= r) && (b <= s)) || ((a <= r) && (c <= s)) || ((b <= r)&&(c <= s)) ||
+                ((a <= s) && (b <= r)) || ((a <= s) && ( c <= r)) || ((b <= s) && (c <= r))
