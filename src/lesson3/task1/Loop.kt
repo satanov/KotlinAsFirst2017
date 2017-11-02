2e5647a52d295aa980c4b7d2ef7d9efb3c6e52b4
Maxim <satanov201354@gmail.com>
2017-11-02 22:50:07 +0300

    Loop 02.11

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -171,7 +171,7 @@ fun isCoPrime(m: Int, n: Int): Boolean {
  * Например, для интервала 21..28 21 <= 5*5 <= 28, а для интервала 51..61 квадрата не существует.
  */
 fun squareBetweenExists(m: Int, n: Int): Boolean {
-    val squareRootForM = sqrt(m.toDouble()).toInt()
+    var squareRootForM = sqrt(m.toDouble()).toInt()
     val squareRootForN = sqrt(n.toDouble()).toInt()
     if (m == 0 && n == 0) return true
     for(i in squareRootForM..squareRootForN) {
