d02b5e6a2ad3631e0be5253e5f2c60d00170161f
Maxim <satanov201354@gmail.com>
2017-12-17 22:51:18 +0300

    List 03.11

--- src/lesson6/task1/Geometry.kt
+++ src/lesson6/task1/Geometry.kt
@@ -132,7 +132,7 @@ fun diameter(vararg points: Point): Segment {
  * Центр её должен находиться посередине между точками, а радиус составлять половину расстояния между ними
  */
 fun circleByDiameter(diameter: Segment): Circle =
-Circle(Point(((diameter.begin.x + diameter.end.x) / 2), (diameter.begin.y + diameter.end.y) / 2),
+        Circle(Point(((diameter.begin.x + diameter.end.x) / 2), (diameter.begin.y + diameter.end.y) / 2),
 diameter.begin.distance(diameter.end) / 2)
 
 /**
